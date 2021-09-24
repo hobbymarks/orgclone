bb8028f035b150ce90409889329c42787e521388
hobbymarks <ihobbymarks@gmail.com>
2021-09-24 16:39:56 +0800

    ...

--- orGClone.sh
+++ orGClone.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 
-if [[ $# -eq 0 ]];then
+if [[ $# -eq 0 ]]
+then
     echo "Please input org parameter" >&2
     exit 2
-elif [[  $# -ne 1 ]];then
+elif [[  $# -ne 1 ]]
+then
     echo "Please input only one org parameter" >&2
     exit 2
 fi
@@ -13,7 +15,8 @@ echo "********** all repos in "$1":"
 
 repos=$(gh repo list $1 | grep $1 | cut -f 1 | cut -d "/" -f 2)
 idx=1
-while IFS= read -r line; do
+while IFS= read -r line
+do
     echo "-->"$idx:$line
     ((idx = idx + 1))
     git clone https://github.com/$1/$line.git
