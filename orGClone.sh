45b4db44abb8ebb851a3fbea8587a854bc800df3
hobbymarks <ihobbymarks@gmail.com>
2021-09-24 16:36:00 +0800

    Feature:add parameters count check

--- orGClone.sh
+++ orGClone.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+if [[ $# -eq 0 ]];then
+    echo "Please input org parameter" >&2
+    exit 2
+elif [[  $# -ne 1 ]];then
+    echo "Please input only one org parameter" >&2
+    exit 2
+fi
+
 echo "********** all repos in "$1":"
 
 repos=$(gh repo list $1 | grep $1 | cut -f 1 | cut -d "/" -f 2)
