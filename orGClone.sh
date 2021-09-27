bab0a9952487306fb06ba2bb1237c03057244310
hobbymarks <ihobbymarks@gmail.com>
2021-09-27 14:26:13 +0800

    Feature: add options

--- orGClone.sh
+++ orGClone.sh
@@ -1,23 +1,79 @@
 #!/bin/bash
+################################################################################
+# Help                                                                         #
+################################################################################
+Help()
+{
+    # Display Help
+    echo "git clone all the repos of an organization."
+    echo
+    echo "Syntax: orgClone.sh [-l NAME | -r NAME |h]"
+    echo "options:"
+    echo "l     List all repos in the NAME organization one bye one."
+    echo "r     Run git clone repo in the NAME organization one bye one."
+    echo "h     Print this Help."
+    #echo "v     Verbose mode."
+    #echo "V     Print software version and exit."
+    echo
+}
 
+################################################################################
+################################################################################
+# Main program                                                                 #
+################################################################################
+################################################################################
 
-if [[ $# -eq 0 ]]
-then
-    echo "Please input org parameter" >&2
-    exit 2
-elif [[  $# -ne 1 ]]
-then
-    echo "Please input only one org parameter" >&2
-    exit 2
-fi
+# Orgnization Name
+orgName=""
 
-echo "********** all repos in "$1":"
-
-repos=$(gh repo list $1 | grep $1 | cut -f 1 | cut -d "/" -f 2)
-idx=1
-while IFS= read -r line
-do
-    echo "-->"$idx:$line
-    ((idx = idx + 1))
-    git clone https://github.com/$1/$line.git
-done <<<"$repos"
+################################################################################
+# List all repos in the org
+################################################################################
+ListRepos()
+{
+     echo "********** all repos in $orgName:"
+     repos=$(gh repo list $orgName | grep $orgName | cut -f 1 | cut -d "/" -f 2)
+     idx=1
+     while IFS= read -r line
+     do
+         echo "-->"$idx:$line
+         ((idx = idx + 1))
+     done <<<"$repos"
+}
+################################################################################
+# Git clone all repos in the org
+################################################################################
+CloneRepos()
+{
+     echo "********** all repos in $orgName:"
+     repos=$(gh repo list $orgName | grep $orgName | cut -f 1 | cut -d "/" -f 2)
+     idx=1
+     while IFS= read -r line
+     do
+         echo "-->"$idx:$line
+         ((idx = idx + 1))
+         git clone https://github.com/$orgName/$line.git
+     done <<<"$repos"
+}
+################################################################################
+# Process the input options.                                                   #
+################################################################################
+# Get the options
+while getopts ":hr:l:" option; do
+    case $option in
+        h) # Display Help
+            Help
+            exit;;
+        r) # Git clone all repos in the org
+            orgName=$OPTARG
+            CloneRepos
+            exit;;
+        l) # List all repos
+            orgName=$OPTARG
+            ListRepos
+            exit;;
+        \?) # Invalid option
+            echo "Error: Invalid option."
+            exit;;
+    esac
+done
